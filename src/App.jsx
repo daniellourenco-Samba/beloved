3152de88fc4cb28425745e903b425858a2511f21
Daniel Torres <daniellourenco30@gmail.com>
2023-05-15 01:28:14 -0300

    committ inicial

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import './App.css';
+import img1 from './data/foto1.png'
+
+function App() {
+
+  const url = "https://gift-repo-eurawdqtt-daniellotorres.vercel.app/razoes";  
+  const [razoes, setrazoes] = useState([]);
+
+
+  const click = 
+    async function fetchData() {
+      console.log('buscando')
+      const res = await fetch(url);
+
+      const data = await res.json();
+      setrazoes(data);
+  }
+
+  return (
+    <div className="App">
+     <h1>Aqui será o app</h1>
+      <img src={img1} alt="imagem1" />
+      <ul>
+        {razoes.map((razao) => (
+          <li key={razao.id}>{razao.texto}</li>
+        ))}
+      </ul>
+      <button onClick={click}>Clique aqui</button>
+    </div>
+  );
+}
+
+export default App;
+
+    
