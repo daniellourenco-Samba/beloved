448a1b85bdf013707d1cc48a5dbf1e36875f488d
Daniel Torres <daniellourenco30@gmail.com>
2023-05-23 21:03:59 -0300

    editado

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,7 +12,7 @@ const Card = (props) => {
     setRazao([])
     let infos = document.querySelector('.card')
     infos.innerHTML = 
-    `<h1>Eu te amo porque</h1><h2>Você me transmite a maior segurança do mundo!</h2>`
+    `<h1>Você é incrivelmente bondosa, tenho orgulho disso!</h1>`
     await axios({
         method: "get",
           url: url
@@ -25,13 +25,7 @@ const Card = (props) => {
     <div>
         <div className="card" onClick={(e) => {carregaRazao()}}>
             <h2>Oi Mo!</h2>
-            <h2>Finalmente a espera acabou, seu presente está aqui!</h2>
-            <h3>Lembra da dica do Bereal ? <br />
-              Esse é o Beloved, todo dia ele vai te mandar uma mensagem proce vir aqui.
-              Quero que você lembre como eu te amo, então diariamente eu vou te dizer um motivo pelo qual te amo!
-            </h3>
-            <h5>Também teremos algumas surpresas nesse app aqui rs</h5>
-            <h6>Agora clica no card pra ver um motivo pelo qual te amo</h6>
+            <h2>Eu te amo porque</h2>
         </div>
     </div>
   )
