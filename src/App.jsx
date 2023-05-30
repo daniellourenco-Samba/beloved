11f6d0c35b2563161ba6cb708d7f8bc3c47a569f
DanieLOTorres <daniellourenco30@gmail.com>
2023-05-30 16:17:35 -0300

    Adding Logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 
 import Card from './components/Card'
+import Logo from './data/logo.svg'
 
 function App() {
   
   return (
     <div className="App">
+      <img src={Logo} alt="Beloved." className='logo'/>
       <Card></Card>
   </div>
   );
