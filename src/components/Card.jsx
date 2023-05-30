427c137ff061458f64432250fcdfc5c17b4c833a
Daniel Torres <53536633+DanielLOTorres@users.noreply.github.com>
2023-05-30 00:00:08 -0300

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,7 +12,7 @@ const Card = (props) => {
     setRazao([])
     let infos = document.querySelector('.card')
     infos.innerHTML = 
-    `<h1>Você é dedicada</h1>`
+    `<h1>Nossas tradições. Pedido!</h1>`
     await axios({
         method: "get",
           url: url
@@ -25,7 +25,7 @@ const Card = (props) => {
     <div>
         <div className="card" onClick={(e) => {carregaRazao()}}>
             <h2>Oi Mo!</h2>
-            <h2>Eu te amo porque...</h2>
+            <h2>Uma coisa que eu amo é...</h2>
         </div>
     </div>
   )
