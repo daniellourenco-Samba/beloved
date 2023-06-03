9f60ff42fe50175c30cb141054bf5a7a667df1a9
Daniel Torres <53536633+DanielLOTorres@users.noreply.github.com>
2023-06-02 22:31:24 -0300

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,13 +12,7 @@ const Card = (props) => {
     setRazao([])
     let infos = document.querySelector('.card')
     infos.innerHTML = 
-    ` <h1>"And I will love you, baby, always
-    And I'll be there forever and a day, always
-    I'll be there 'til the stars don't shine
-    'Til the heavens burst and the words don't rhyme
-    And I know when I die, you'll be on my mind
-    And I'll love you, always"</h1>
-    <h2>É essa agui mo <a href="https://open.spotify.com/intl-pt/track/2RChe0r2cMoyOvuKobZy44?autoplay=true">Musga</a></h2>`
+    ` <h1>Oce cuida di eu (i eu sempe vo cuidar do ce)</h1>`
     
     await axios({
         method: "get",
@@ -32,7 +26,7 @@ const Card = (props) => {
     <div>
         <div className="card" onClick={(e) => {carregaRazao()}}>
             <h2>Oi Mo!</h2>
-            <h2>Hoje eu vou te dizer o trecho de uma musga...</h2>
+            <h2>Eu te amo puque...</h2>
         </div>
     </div>
   )
